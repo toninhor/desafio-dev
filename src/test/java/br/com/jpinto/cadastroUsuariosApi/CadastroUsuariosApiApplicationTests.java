8cfc14073fc7bd622d3c06451404f8d97157173b
José Antonio Pinto <jose@jpinto.com.br>
2021-05-20 23:44:26 -0300

    Adiciona Spring Boot Starter

---
+++ src/test/java/br/com/jpinto/cadastroUsuariosApi/CadastroUsuariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jpinto.cadastroUsuariosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
